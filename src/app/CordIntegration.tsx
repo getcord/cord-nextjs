5b9ab5f4e370e0bbce02c7b37da6c30ea44073b7
ludo <ludo@ludos-MacBook-Pro.local>
2023-07-28 17:12:41 +0200

    No user switch

--- src/app/CordIntegration.tsx
+++ src/app/CordIntegration.tsx
@@ -4,20 +4,15 @@ import { CordProvider } from "@cord-sdk/react";
 
 type Props = {
   clientAuthToken: string | null;
-  users: string[];
-  userIndex: number;
 };
 const CordIntegration = ({
   clientAuthToken,
-  users,
-  userIndex,
   children,
 }: React.PropsWithChildren<Props>) => {
   return (
     <div>
       {clientAuthToken ? (
         <CordProvider clientAuthToken={clientAuthToken}>
-          <ChangeUser users={users} userIndex={userIndex} />
           {children}
         </CordProvider>
       ) : (
@@ -32,41 +27,12 @@ const CordIntegration = ({
             <li>Paste your Application ID and Secret in it.</li>
             <pre>{`CORD_APP_ID=<Application ID>
 CORD_SECRET=<Secret>`}</pre>
-            <li>Restart your remix</li>
+            <li>Restart your next.js</li>
           </ol>
         </div>
       )}
     </div>
   );
 };
-
-function ChangeUser({
-  users,
-  userIndex,
-}: {
-  users: string[];
-  userIndex: number;
-}) {
-  return (
-    <label className="change-user">
-      Change user
-      <select
-        value={userIndex}
-        onChange={(e) => {
-          const newUserIndex = e.target.value;
-          const searchParams = new URLSearchParams(location.search);
-          searchParams.set("userIndex", newUserIndex);
-          location.search = searchParams.toString();
-        }}
-      >
-        {users.map((user, idx) => (
-          <option key={idx} value={idx}>
-            {user}
-          </option>
-        ))}
-      </select>
-    </label>
-  );
-}
 
 export default CordIntegration;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,80 +1,5 @@
 import ClientThread from "./ClientThread";
-import CordIntegration from "./CordIntegration";
-import styles from "./page.module.css";
 
-export async function getData(requestedUserIndex: number) {
-  // @ts-ignore I need this here, so it is no included in the client bundle
-  // It should not be needed because remix does Server Code Pruning.
-  const { getClientAuthToken } = await import("@cord-sdk/server");
-
-  const { CORD_SECRET, CORD_APP_ID } = process.env;
-  if (!CORD_SECRET || !CORD_APP_ID) {
-    console.error(
-      "Missing CORD_SECRET or CORD_ORD_ID env variable. Get it on console.cord.com and add it to .env"
-    );
-    return { clientAuthToken: null, users: [], userIndex: -1 };
-  }
-  let userIndex = requestedUserIndex;
-  if (userIndex === null || userIndex === undefined || isNaN(userIndex)) {
-    userIndex = Math.round(Math.random() * 3);
-  }
-
-  const users = [
-    {
-      // The user ID can be any identifier that makes sense to your application.
-      // As long as it's unique per-user, Cord can use it to represent your user.
-      user_id: "severusatreides",
-
-      // Same as above. An organization ID can be any unique string. Organizations
-      // are groups of users.
-      organization_id: "starpotterdunewars",
-      user_details: {
-        email: "sevvy@arrakis.spice",
-        name: "Severus Atreides",
-      },
-    },
-    {
-      user_id: "minervahalleck",
-      organization_id: "starpotterdunewars",
-      user_details: {
-        email: "catlady@tattoine.gov",
-        name: "Minerva Halleck",
-      },
-    },
-    {
-      user_id: "hermioneorgana",
-      organization_id: "starpotterdunewars",
-      user_details: {
-        email: "hermi1979@starfleet.org.terra",
-        name: "Hermione Organa",
-      },
-    },
-    {
-      user_id: "jarjarmarvolo",
-      organization_id: "starpotterdunewars",
-      user_details: {
-        email: "meesa@lowkey.sith",
-        name: "Jar Jar Marvolo",
-      },
-    },
-  ];
-  const safeUserIndex = userIndex % users.length;
-  const user = users[safeUserIndex];
-
-  const clientAuthToken = getClientAuthToken(CORD_APP_ID, CORD_SECRET, {
-    ...user,
-    // By supplying the `organization_details` object, just like the user,
-    // Cord will create the organization on-the-fly.
-    organization_details: {
-      name: "starpotterdunewars",
-    },
-  });
-  return {
-    clientAuthToken,
-    users: users.map((user) => user.user_details.name),
-    userIndex: safeUserIndex,
-  };
-}
 export default async function Home({
   searchParams,
 }: {
@@ -84,14 +9,5 @@ export default async function Home({
   if (isNaN(userIndex)) {
     userIndex = Math.round(Math.random() * 3);
   }
-  const { clientAuthToken, users } = await getData(userIndex);
-  return (
-      <CordIntegration
-        clientAuthToken={clientAuthToken}
-        users={users}
-        userIndex={userIndex}
-      >
-        <ClientThread />
-      </CordIntegration>
-  );
+  return <ClientThread />;
 }
