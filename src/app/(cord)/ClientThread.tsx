720c5ed5723d4e0beb66cc5a341bfee45305d4d7
Ludo <ludo@cord.com>
2023-08-17 09:37:25 +0200

    Add some comments about events and components

--- src/app/(cord)/ClientThread.tsx
+++ src/app/(cord)/ClientThread.tsx
@@ -2,6 +2,10 @@
 
 import { PagePresence, Thread } from "@cord-sdk/react";
 
+/**
+ * We are adding page presence and a thread.
+ * You can add more collaboration feature, see [components](https://docs.cord.com/components).
+ **/
 export default function ClientThread() {
   return (
     <div style={{ margin: "30px auto", maxWidth: "500px" }}>
