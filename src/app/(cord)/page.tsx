69d454a4dc56c990d137eafc2869ed2c54a35249
Ludo <ludo@cord.com>
2023-10-26 12:42:15 +0800

    store user in cookie + user selector

--- src/app/(cord)/page.tsx
+++ src/app/(cord)/page.tsx
@@ -1,5 +1,14 @@
+import { CORD_USER_COOKIE, USERS } from "@/consts";
 import ClientThread from "./ClientThread";
+import { cookies } from "next/headers";
+async function getData() {
+  const userIdCookie = cookies().get(CORD_USER_COOKIE)?.value;
+  const userIndex =
+    USERS.findIndex((user) => user.user_id === userIdCookie) ?? 0;
+  return { users: USERS.map((user) => user.user_id), userIndex };
+}
 
 export default async function HomeWithThread() {
-  return <ClientThread />;
+  const { users, userIndex } = await getData();
+  return <ClientThread users={users} userIndex={userIndex} />;
 }
